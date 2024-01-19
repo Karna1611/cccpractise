fafb528094479f51424043d910eddefa0c0410bf
Your Name <you@example.com>
2024-01-19 09:06:53 +0530

    String exercise

--- strings.php
+++ strings.php
@@ -62,8 +62,61 @@
     // echo strrev("Lionel Messi");
 
     // echo str_shuffle("Jai Shree Ram");
-   
     
+    // print_r(str_split("Karna"))."<br>";
+    // echo "<br>";
+    // print_r(str_split("Karna",3))."<br>";
+
+    // echo str_word_count("Hello World")."<br>";
+    // print_r(str_word_count("Hello World",1));
+    // echo "<br>";
+    // print_r(str_word_count("Hello World",2));
+    // echo "<br>";
+    //Index 1 Return an array with the words from the string:
+    //Index 2 Return an array where the key is the position of the word in the string, and value is the actual word:
+
+    // echo substr_replace("Hello","world",0)."<br>";
+    // echo substr_replace("Hello world","earth",6)."<br>";
+    // echo substr_replace("world","Hello ",0,0)."<br>";
+    // $replace = array("1: AAA","2: AAA","3: AAA");
+    // echo implode("<br>",substr_replace($replace,'BBB',3,3))."<br>";
+
+    // $name="John";
+    // echo str_pad($name,6,"_",STR_PAD_LEFT)."<br>";
+    // echo str_pad($name,8,"*")."<br>";
+    //By default padding is right
+
+    //setlocale (LC_COLLATE, 'NL');
+    // echo strcoll("Hello World!","Hello World!");
+    // echo "<br>";
+
+    // echo strcoll("Hello World!","Karna Trivedi!");
+    // echo "<br>";
+    //<0 - if string1 is less than string2
+    //>0 - if string1 is greater than string2
+
+    //Print the number of characters found in "Hello world!" before the character "w":
+    //echo strcspn("Hello world!","w")."<br>";
+
+    //Find the first occurrence of "world" inside "Hello world!", and return the rest of the string:
+    // echo stristr("Hello world!","WORLD")."<br>";
+    // //Search a string for the ASCII value of "o", and return the rest of the string:
+    // echo stristr("Hello world!",111);
+    // //Return the part of the string before the first occurence of "world":
+    // echo stristr("Hello world!","WORLD",true);   
+    
+    //The strrev() function reverses a string.
+    //echo strrev("Hello World!")."<br>";
+
+    //The ucfirst() function converts the first character of a string to uppercase.
+    //echo ucfirst("hello world!")."<br>";
+    // lcfirst() - converts the first character of a string to lowercase
+    // ucwords() - converts the first character of each word in a string to uppercase
+    // strtoupper() - converts a string to uppercase
+    // strtolower() - converts a string to lowercase
+
+    //echo ucwords("hello world");
+
 ?>
 
 
