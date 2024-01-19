87059027d28ee3a24f9b44fab4003e34115e73bd
Your Name <you@example.com>
2024-01-19 10:34:58 +0530

    String Homework

---
+++ data_types.php
@@ -0,0 +1,18 @@
+<?php
+    //The var_dump() function returns the data type and the value:
+    $x=10;
+    var_dump($x);
+    echo "<br>";
+    $x="Hello World";
+    $y="Good morning!";
+    var_dump($y);
+    echo "<br>";
+    $x=10.65;
+    var_dump($x);
+    echo "<br>";
+    $x=true;
+    var_dump($x);
+    echo "<br>";
+    $x=array("Rohit","Gill","Kohli");
+    var_dump($x);
+?>
---
+++ type_casting.php
@@ -0,0 +1,169 @@
+<?php
+    $x=10;
+    $y=56.78;
+    $c="Karna";
+    $d=true;
+    $e=null;
+    $x=(string) $x;
+    var_dump($x);
+    echo "<br>";
+    $y=(string) $y;
+    var_dump($y);
+    echo "<br>";
+    $c=(string) $c;
+    var_dump($c);
+    echo "<br>";
+    $d=(string) $d;
+    var_dump($d);
+    echo "<br>";
+    $e=(string) $e;
+    var_dump($e);
+    echo "<br>";
+
+    $a = 5;       
+    $b = 5.34;    
+    $c = "25 kilometers"; 
+    $d = "kilometers 25"; 
+    $e = "hello"; 
+    $f = false;    
+    $g = NULL;    
+
+    $a = (int) $a;
+    $b = (int) $b;
+    $c = (int) $c;
+    $d = (int) $d;
+    $e = (int) $e;
+    $f = (int) $f;
+    $g = (int) $g;
+    var_dump($a);
+    echo "<br>";
+    var_dump($b);
+    echo "<br>";
+    var_dump($c);
+    echo "<br>";
+    var_dump($d);
+    echo "<br>";
+    var_dump($e);
+    echo "<br>";
+    var_dump($f);
+    echo "<br>";
+    var_dump($g);
+    echo "<br>";
+
+    $a = 5;       
+    $b = 5.34;    
+    $c = "25 kilometers"; 
+    $d = "kilometers 25"; 
+    $e = "hello"; 
+    $f = false;    
+    $g = NULL;    
+
+    $a = (float) $a;
+    $b = (float) $b;
+    $c = (float) $c;
+    $d = (float) $d;
+    $e = (float) $e;
+    $f = (float) $f;
+    $g = (float) $g;
+
+    var_dump($a);
+    echo "<br>";
+    var_dump($b);
+    echo "<br>";
+    var_dump($c);
+    echo "<br>";
+    var_dump($d);
+    echo "<br>";
+    var_dump($e);
+    echo "<br>";
+    var_dump($f);
+    echo "<br>";
+    var_dump($g);
+    echo "<br>";
+
+    $a = 5;       
+    $b = 5.34;    
+    $c = "25 kilometers"; 
+    $d = "kilometers 25"; 
+    $e = "hello"; 
+    $f = false;    
+    $g = NULL;    
+    $h = true;
+    $i = NULL; 
+
+    $a = (bool) $a;
+    $b = (bool) $b;
+    $c = (bool) $c;
+    $d = (bool) $d;
+    $e = (bool) $e;
+    $f = (bool) $f;
+    $g = (bool) $g;
+    $h = (bool) $h;
+    $i = (bool) $i;
+
+    var_dump($a);
+    echo "<br>";
+    var_dump($b);
+    echo "<br>";
+    var_dump($c);
+    echo "<br>";
+    var_dump($d);
+    echo "<br>";
+    var_dump($e);
+    echo "<br>";
+    var_dump($f);
+    echo "<br>";
+    var_dump($g);
+    echo "<br>";
+    var_dump($h);
+    echo "<br>";
+    var_dump($i);
+    echo "<br>";
+
+    $a = 5;       
+    $b = 5.34;   
+    $c="Hello";
+    $d=true;
+    $e=NULL;
+
+    $a = (array) $a;
+    $b = (array) $b;
+    $c = (array) $c;
+    $d = (array) $d;
+    $e = (array) $e;
+
+    var_dump($a);
+    echo "<br>";
+    var_dump($b);
+    echo "<br>";
+    var_dump($c);
+    echo "<br>";
+    var_dump($d);
+    echo "<br>";
+
+    $a = 5;       
+    $b = 5.34;   
+    $c="Hello";
+    $d=true;
+    $e=NULL;
+
+    // $a = (unset) $a;
+    // $b = (unset) $b;
+    // $c = (unset) $c;
+    // $d = (unset) $d;
+    // $e = (unset) $e;
+
+    // var_dump($a);
+    // echo "<br>";
+    // var_dump($b);
+    // echo "<br>";
+    // var_dump($c);
+    // echo "<br>";
+    // var_dump($d);
+    // echo "<br>";
+    // var_dump($e);
+    // echo "<br>";
+
+    //Unset() function is deprecated after PHP 8.0
+
+?>
